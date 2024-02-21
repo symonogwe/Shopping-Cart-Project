2f9052070b8a56e75f0006aff51d00c34854afcc
Symon Ogwe <symonogwe@gmail.com>
2024-02-21 11:19:24 +0300

    Create hook for fetching all products

---
+++ src/Components/Hooks/useData.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+import axios, { CanceledError } from "axios";
+
+const useData = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get("https://fakestoreapi.com/products")
+      .then((res) => {
+        console.log(res.data);
+        setData(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setLoading(false);
+      });
+  }, []);
+
+  return { data, loading, error };
+};
+
+export default useData;
