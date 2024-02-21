2ba3923d78cc687ffad44431b260d1a0aa330602
Symon Ogwe <symonogwe@gmail.com>
2024-02-22 00:26:33 +0300

    Add table design in cart page

--- src/Components/CartPage/Cart.jsx
+++ src/Components/CartPage/Cart.jsx
@@ -1,22 +1,58 @@
 import { useContext } from "react";
 import { CartContext } from "../MainApp/App";
-import { HStack, Heading, VStack, Image, Box, Text } from "@chakra-ui/react";
+import {
+  Heading,
+  VStack,
+  Image,
+  TableContainer,
+  Table,
+  TableCaption,
+  Thead,
+  Tr,
+  Th,
+  Tbody,
+  Td,
+} from "@chakra-ui/react";
 
 const Cart = () => {
   const { cart } = useContext(CartContext);
 
   return (
     <VStack w={"100%"}>
-      <Heading mb={5}>MY SHOPPING CART</Heading>
-      <Box w={"100%"} minHeight={"500px"}>
-        {cart.map((cartObj) => (
-          <HStack key={cartObj.id} justifyContent={"space-between"} mb={6}>
-            <Image src={cartObj.image} boxSize={"100px"} />
-            <Text>{cartObj.title}</Text>
-            <Text>${cartObj.count * cartObj.price}</Text>
-          </HStack>
-        ))}
-      </Box>
+      <Heading mb={5} color={"#00a773"}>
+        MY SHOPPING CART
+      </Heading>
+      <TableContainer>
+        <Table>
+          <TableCaption>My Cart Items</TableCaption>
+          <Thead>
+            <Tr>
+              <Th color={"#00a773"}>Item Image</Th>
+              <Th color={"#00a773"}>Item Title</Th>
+              <Th color={"#00a773"}>Item Count</Th>
+              <Th color={"#00a773"} isNumeric>
+                Item Pricing
+              </Th>
+              <Th color={"#00a773"} isNumeric>
+                Amount
+              </Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {cart.map((object) => (
+              <Tr key={object.id}>
+                <Td>
+                  <Image src={object.image} boxSize={"100px"} />
+                </Td>
+                <Td>{object.title}</Td>
+                <Td>{object.count}</Td>
+                <Td>{object.price}</Td>
+                <Td>{object.price * object.count}</Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </TableContainer>
     </VStack>
   );
 };
